d1262d57749b26ad2ed08954a5225c3403463fc6
Komagal Meenatchi B M <komagalmeenatchi24@gmail.com>
2026-02-27 15:06:24 +0530

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -106,21 +106,22 @@ if st.button("Predict"):
     df = pd.DataFrame([new_data])
     file_path = "employee_predictions.csv"
 
-    if os.path.exists(file_path):
-       df.to_csv(file_path, mode="a", header=False, index=False)
-    else:
-       df.to_csv(file_path, index=False)
-
-       st.success(" Data Saved Successfully!")
-
-       st.header("Dashboard")
-
-       if os.path.exists("employee_predictions.csv"):
-          data = pd.read_csv("employee_predictions.csv")
-          st.dataframe(data)
+       if os.path.exists(file_path):
+          df.to_csv(file_path, mode="a", header=False, index=False)
        else:
-           st.info("No predictions saved yet.")
+          df.to_csv(file_path, index=False)
+
+          st.success(" Data Saved Successfully!")
+
+          st.header("Dashboard")
+
+          if os.path.exists("employee_predictions.csv"):
+             data = pd.read_csv("employee_predictions.csv")
+             st.dataframe(data)
+          else:
+              st.info("No predictions saved yet.")
     
+
 
 
 
