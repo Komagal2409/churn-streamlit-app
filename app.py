a43628099db5960ca984ebc3c60e5692e3a7b5ad
Komagal Meenatchi B M <komagalmeenatchi24@gmail.com>
2026-02-27 12:18:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 import streamlit as st
-import pickle
-import numpy as np
+import pandas as pd
+import joblib
 
-# Load model
-model = pickle.load(open("churn_model.pkl", "rb"))
+model = joblib.load("churn_model (1).pkl")
+cols = joblib.load("model_columns (1).pkl")
 
-st.title("Customer Churn Prediction")
+st.title("Churn Prediction")
 
-credit_score = st.number_input("Credit Score")
-age = st.number_input("Age")
-balance = st.number_input("Balance")
-isactive = st.number_input("IsActiveMember")
-salary = st.number_input("Estimated Salary")
-Female = st.number_input("Gender_Female")
-Male = st.number_input("Gender_Male")
-France = st.number_input("Geography_France")
-Germany = st.number_input("Geography_Germany")
-Spain = st.number_input("Geography_Spain")
+CreditScore = st.number_input("CreditScore")
+Age = st.number_input("Age")
+Balance = st.number_input("Balance")
+IsActiveMember = st.selectbox("IsActiveMember",[0,1])
+EstimatedSalary = st.number_input("EstimatedSalary")
+
+Gender = st.selectbox("Gender",["Male","Female"])
+Geography = st.selectbox("Geography",["France","Germany","Spain"])
+
+input_dict = {
+    "CreditScore":CreditScore,
+    "Age":Age,
+    "Balance":Balance,
+    "IsActiveMember":IsActiveMember,
+    "EstimatedSalary":EstimatedSalary,
+    "Gender_Female":1 if Gender=="Female" else 0,
+    "Gender_Male":1 if Gender=="Male" else 0,
+    "Geography_France":1 if Geography=="France" else 0,
+    "Geography_Germany":1 if Geography=="Germany" else 0,
+    "Geography_Spain":1 if Geography=="Spain" else 0
+}
+
+input_df = pd.DataFrame([input_dict])
+input_df = input_df[cols]
 
 if st.button("Predict"):
-    features = np.array([[credit_score, age, balance, salary, isactive, Female, Male, France, Spain, Germany]])
-    prediction = model.predict(features)
-
-    if prediction[0] == 1:
-        st.error("Customer will churn ❌")
-    else:
-        st.success("Customer will stay ✅")
-
+    pred = model.predict(input_df)
+    st.write("Churn" if pred[0]==1 else "No Churn")
